6c1ce41cc910310a073d4bb7aef5e83458d3cdba
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-11-29 16:16:13 +0530

    Working with MongoDb Atlas using mongoose

--- node-rest-tutorial/api/routes/products.js
+++ node-rest-tutorial/api/routes/products.js
@@ -1,47 +1,106 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/',(req,res,next)=>{
-    res.status(200).json({
-        message : 'Handling Get Requests'
-    });
+    Product.find().exec()
+        .then(docs => {
+            console.log(docs);
+            // if(docs.length>=0){
+                res.status(200).json(docs);
+            // }else{
+            //     res.status(404).json({
+            //         message: 'No entries found'
+            //     });
+            // }
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.post('/',(req,res,next)=>{
-    const product = {
+    const product = new Product({
+        _id: mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
-        message : 'Handling Post Requests',
-        createdProduct:product
+    });
+    product.save()
+    .then(result=>{
+        console.log(result)
+        res.status(201).json({
+            message : 'Handling Post Requests',
+            createdProduct:product
+        });
+    })
+    .catch(err=>{
+        console.log(err)
+        res.status(500).json({
+            error: err
+        });
     });
 });
 
-router.post('/:productId',(req,res,next)=>{
+router.get('/:productId',(req,res,next)=>{
     const id = req.params.productId;
-    if(id === 'special'){
-        res.status(200).json({
-            message : 'You discovered an Special Id',
-            id:id
+    Product.findById(id).exec()
+        .then(doc=>{
+            console.log(doc)
+            if(doc){
+                res.status(200).json(doc);
+            }
+            else {
+                res.status(404).json({
+                    error:'No valid entry found'
+                });
+            }
         })
-    }else {
-        res.status(200).json({
-            message:'You passed an Id'
-        })
-    }
+        .catch(err=>{
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 router.patch('/:productId',(req,res,next)=>{
-    res.status(200).json({
-        message : 'Updated Product'
-    })
+    const id = req.params.productId;
+    const updateOps = {};
+    for(const ops of req.body){
+        updateOps[ops.propName] = ops.value;
+    }
+
+    Product.update({_id: id},{$set: updateOps}).exec()
+        .then(result=>{
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err=>{
+            console.log(err);
+            res.status(500).json({
+                error:err
+            });
+        });
 });
 
 router.delete('/:productId',(req,res,next)=>{
-    res.status(200).json({
-        message: 'Deleted Product'
-    })
+    const id = req.params.productId;
+    Product.remove({_id:id}).exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 module.exports = router;
--- node-rest-tutorial/app.js
+++ node-rest-tutorial/app.js
@@ -2,16 +2,32 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
+const mongoose = require('mongoose');
+
+const pass = 'rLyjQJBmOl1RMI2e';
 const orderRoutes = require('./api/routes/orders');
 const bodyParser = require('body-parser');
 
+mongoose.connect('mongodb+srv://arjupta:rLyjQJBmOl1RMI2e@node-rest.tuzw8.mongodb.net/node-rest?retryWrites=true&w=majority');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
+app.use((req,res,next)=>{
+    res.header('Access-Control-Allow-Origin','*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 // Routes to direct
 app.use('/products',productRoutes);
-
 app.use('/orders',orderRoutes);
 
 app.use((req,res,next)=>{
