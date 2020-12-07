2a693bf25a98bbf9733afddd3108a5df28625025
ARJUPTA <64064110+ARJUPTA@users.noreply.github.com>
2020-12-07 23:39:09 +0530

    Uploading Files via multer while posting a product.

--- node-rest-tutorial/app.js
+++ node-rest-tutorial/app.js
@@ -11,6 +11,7 @@ const bodyParser = require('body-parser');
 mongoose.connect('mongodb+srv://arjupta:rLyjQJBmOl1RMI2e@node-rest.tuzw8.mongodb.net/node-rest?retryWrites=true&w=majority');
 mongoose.Promise = global.Promise;
 app.use(morgan('dev'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
